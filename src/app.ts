466065954180c2a7a52bcbf182340e87af226dce
Yevhenii <yevmot01@easv365.dk>
2025-02-03 13:07:15 +0100

    made conection with db, created conection, interfaces, models

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,25 @@
 import express, { Application, Request, Response } from "express";
 import dotenvFlow from "dotenv-flow";
+import { testConnection } from "./repository/database";
 import routes from "./routes";
 
 dotenvFlow.config();
 
-// Create Express application
+// create express application
 const app: Application = express();
 
-// Routes
 app.use("/api", routes);
 
-// Middleware
+/**
+ *
+ */
 export function startServer() {
+  // test the connection to the database
+  testConnection();
+
+  // start the server
   const PORT: number = parseInt(process.env.PORT as string) || 4000;
-  // Start server
   app.listen(PORT, function () {
-    console.log("Server is up and running on port " + PORT);
+    console.log("Server is up and running on port: " + PORT);
   });
 }
